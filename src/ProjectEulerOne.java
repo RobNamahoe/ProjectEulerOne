c025f71494a9c1b61baa45e6b518d89a4dc7dedb
Rob Namahoe <rnamahoe@hawaii.edu>
2015-02-03 15:01:44 -1000

    Finished Wod

---
+++ src/ProjectEulerOne.java
@@ -0,0 +1,16 @@
+/**
+ * Created by rckndn on 2/3/15.
+ */
+public class ProjectEulerOne {
+
+  public static void main(String[] args) {
+   int sum = 0;
+    for (int i = 0; i < 1000; i++) {
+      if ( (i % 3 == 0) || (i % 5 == 0) ) {
+        sum+=i;
+      }
+    }
+    System.out.println("sum: " + sum);
+  }
+
+}
